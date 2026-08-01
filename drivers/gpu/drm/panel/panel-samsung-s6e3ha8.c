3089b8f9e24c37d740d13bae18cb58810646df15
David Heidelberg <david@ixit.cz>
2026-08-01 19:55:23 +0200

    drm/panel: s6e3ha8: Introduce AMB577PX01 panel compatible
    S6E3HA8 is the DDIC, while the panel used on the Samsung Galaxy S9 is
    the Samsung AMB577PX01. Match on the new panel compatible, and keep
    the deprecated DDIC-only one working for existing devicetrees.
    
    Signed-off-by: David Heidelberg <david@ixit.cz>

--- drivers/gpu/drm/panel/panel-samsung-s6e3ha8.c
+++ drivers/gpu/drm/panel/panel-samsung-s6e3ha8.c
@@ -322,7 +322,8 @@ static void s6e3ha8_amb577px01_wqhd_remove(struct mipi_dsi_device *dsi)
 }
 
 static const struct of_device_id s6e3ha8_amb577px01_wqhd_of_match[] = {
-	{ .compatible = "samsung,s6e3ha8" },
+	{ .compatible = "samsung,s6e3ha8" }, /* deprecated */
+	{ .compatible = "samsung,s6e3ha8-amb577px01" },
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, s6e3ha8_amb577px01_wqhd_of_match);
